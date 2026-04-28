6626179f7f854af22b9d375a3714918d1dacc0f5
Kevin Riley <84453350+kmacim@users.noreply.github.com>
2026-04-27 17:00:28 -0700

    image update 3

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function toggleMenu() {
     const nav = document.getElementById("navMenu");
     nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 }
+
+// For top scrolling buttoon
+const [href="#top"] = document.getElementById(["href=#top"]);
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
